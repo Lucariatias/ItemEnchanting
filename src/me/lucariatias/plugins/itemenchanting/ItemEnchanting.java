4417830eaefde332433a5471a17ace99f91748b8
Lucariatias <lucariatias@gmail.com>
2013-04-25 20:33:34 +0100

    Added Metrics

--- src/me/lucariatias/plugins/itemenchanting/ItemEnchanting.java
+++ src/me/lucariatias/plugins/itemenchanting/ItemEnchanting.java
@@ -1,10 +1,18 @@
 package me.lucariatias.plugins.itemenchanting;
 
+import java.io.IOException;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ItemEnchanting extends JavaPlugin {
 	
 	public void onEnable() {
+		try {
+			Metrics metrics = new Metrics(this);
+			metrics.start();
+		} catch (IOException exception) {
+			this.getLogger().warning("Failed to submit stats.");
+		}
 		this.saveDefaultConfig();
 		this.getServer().getPluginManager().registerEvents(new EnchantItemListener(this), this);
 		this.getCommand("itemenchant").setExecutor(new ItemEnchantCommand(this));
